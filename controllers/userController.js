894433acf369f39c81293a16e6b0238c21054de4
PrincyChauhan <chauhanprincee7@gmail.com>
2025-01-29 14:35:52 +0530

    Model and attributes added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,10 @@ const { Post, User } = require("../models");
 
 const oneToOne = async (req, res) => {
   let data = await User.findAll({
+    attributes: ["name", "email"],
     include: {
       model: Post,
+      attributes: ["title", ["name", "postName"]],
     },
     where: { id: 2 },
   });
